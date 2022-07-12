a82948c9004231c2b3740f9e2b03961d0e59e350
junior sousa <92957053+juniors0101@users.noreply.github.com>
2022-07-12 14:07:25 -0300

    up

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -17,7 +17,7 @@ export class ListService {
   }
 
   getAll(): Observable<Animal[]> {
-    return this.http.get<Animal[]>(this.apiUrl)
+    return this.http.get<Animal[]>(this.apiUrl);
   }
 
   getItem(id: number): Observable<Animal> {
